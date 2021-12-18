8a282d90f525f44515bde441d824dd1f9fb4d517
Slimebreath6078 <slimebreath6078@yahoo.co.jp>
2021-12-19 01:55:28 +0900

    出現数フラグを再定義

--- src/system/monster-race-definition.h
+++ src/system/monster-race-definition.h
@@ -6,6 +6,7 @@
 #include "monster-race/race-ability-flags.h"
 #include "monster-race/race-flags-resistance.h"
 #include "monster-race/race-kind-flags.h"
+#include "monster-race/race-population-flags.h"
 #include "system/angband.h"
 #include "util/flag-group.h"
 #include <string>
@@ -67,6 +68,7 @@ struct monster_race {
     EnumClassFlagGroup<MonsterAbilityType> ability_flags; //!< 能力フラグ(魔法/ブレス) / Ability Flags
     EnumClassFlagGroup<MonraceKindType> race_kind_flags; //!< 能力フラグ（種族・徳） / Attr Flags
     EnumClassFlagGroup<MonsterAuraType> aura_flags; //!< オーラフラグ / Aura Flags
+    EnumClassFlagGroup<MonraceKindType> population_flags; //!< 能力フラグ（出現数） / Population Flags
     monster_blow blow[MAX_NUM_BLOWS]{}; //!< 打撃能力定義 / Up to four blows per round
     MONRACE_IDX reinforce_id[6]{}; //!< 指定護衛モンスター種族ID(6種まで)
     DICE_NUMBER reinforce_dd[6]{}; //!< 指定護衛数ダイス数
@@ -106,6 +108,7 @@ struct monster_race {
     EnumClassFlagGroup<MonsterAbilityType> r_ability_flags; //!< 見た能力フラグ(魔法/ブレス) / Observed racial ability flags
     EnumClassFlagGroup<MonraceKindType> r_race_kind_flags; //!< 見た能力フラグ（種族・徳） / Observed racial attr flags
     EnumClassFlagGroup<MonsterAuraType> r_aura_flags; //!< 見た能力フラグ(オーラ) / Observed aura flags
+    EnumClassFlagGroup<MonraceKindType> r_population_flags; //!< 見た能力フラグ（出現数） / Observed population flags
     PLAYER_LEVEL defeat_level{}; //!< 倒したレベル(ユニーク用) / player level at which defeated this race
     REAL_TIME defeat_time{}; //!< 倒した時間(ユニーク用) / time at which defeated this race
     PERCENTAGE cur_hp_per{}; //!< 生成時現在HP率(%)
