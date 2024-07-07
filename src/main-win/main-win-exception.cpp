3ce4d4183890f50a79973005bc3fda2a2c98ff31
Slimebreath6078 <slimebreath6078@yahoo.co.jp>
2024-07-07 18:22:18 +0900

    [Change] エラーが起きた際に送信の代わりにエラーの内容を書き出すようにした

--- src/main-win/main-win-exception.cpp
+++ src/main-win/main-win-exception.cpp
@@ -1,6 +1,41 @@
 #include "main-win/main-win-exception.h"
+#include "io/files-util.h"
 #include "main-win/main-win-utils.h"
+#include "util/angband-files.h"
 #include <sstream>
+
+static void write_error_message(const wchar_t *caption, const std::string& msg)
+{
+
+    const auto path = path_build(ANGBAND_DIR_USER, "error.txt");
+    auto *fp = angband_fopen(path, FileOpenMode::WRITE);
+    if (!fp) {
+        const auto &path_str = path.string();
+#ifdef JP
+        std::wstringstream file_error_msg_ss;
+        file_error_msg_ss
+            << path_str.data() << L" を開くことができませんでした。";
+#else
+        std::wstringstream report_confirm_msg_ss;
+        report_confirm_msg_ss
+            << L"Failed to open " << path_str.data() << ".";
+#endif
+        MessageBoxW(NULL, file_error_msg_ss.str().data(), caption, MB_ICONEXCLAMATION | MB_OK | MB_ICONSTOP);
+        return;
+    }
+
+    // 現在時刻を取得
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+
+    std::stringstream ss = std::stringstream() << std::put_time(std::localtime(&now_c), "%Y-%m-%d %H:%M:%S");
+
+    // フォーマットされた日付と時間を出力
+    fputs((std::string("-----") + ss.str() + "-----\n\n").data(), fp);
+
+    fputs((msg + "\n\n").data(), fp);
+    angband_fclose(fp);
+}
 
 /*!
  * @brief 予期しない例外を処理する
@@ -18,12 +53,11 @@ void handle_unexpected_exception(const std::exception &e)
     std::wstringstream report_confirm_msg_ss;
     report_confirm_msg_ss
         << to_wchar(e.what()).wc_str() << L"\n\n"
-        << _(L"開発チームにエラー情報を送信してよろしいですか？\n", L"Are you sure you want to send the error information to the development team?\n")
-        << _(L"※送信されるのはゲーム内の情報のみであり、個人情報が送信されることはありません。\n",
-               L"Only in-game information will be sent. No personal information will be sent.\n");
+        << _(L"エラー内容をファイルに書き出しますか？\n", L"Are you sure you want to write the error information file?\n");
 
     if (auto choice = MessageBoxW(NULL, report_confirm_msg_ss.str().data(), caption, MB_ICONEXCLAMATION | MB_YESNO | MB_ICONSTOP);
         choice == IDYES) {
+        write_error_message(caption, e.what());
     }
 #endif
 
@@ -35,7 +69,7 @@ void handle_unexpected_exception(const std::exception &e)
 
     if (auto choice = MessageBoxW(NULL, issue_page_open_msg_ss.str().data(), caption, MB_ICONEXCLAMATION | MB_YESNO | MB_ICONSTOP);
         choice == IDYES) {
-        constexpr auto url = "https://github.com/hengband/hengband/issues/new/choose";
+        constexpr auto url = "https://github.com/slimebreath6078/SBband/issues/new/choose";
         ShellExecuteA(NULL, "open", url, NULL, NULL, SW_SHOWNORMAL);
     }
 };
